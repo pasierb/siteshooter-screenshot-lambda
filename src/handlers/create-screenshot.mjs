f35bc4d71e5ef321b4b67762d497daf5cf6569ed
Michal Pasierbski <mpasierbski@gmail.com>
2023-09-20 10:23:18 +0200

    Save screenshots to S3

--- src/handlers/create-screenshot.mjs
+++ src/handlers/create-screenshot.mjs
@@ -1,7 +1,12 @@
 import puppeteer from "puppeteer-core";
 import chromium from "@sparticuz/chromium";
+import { S3Client, PutObjectCommand  } from "@aws-sdk/client-s3";
+
+const s3Client = new S3Client({ region: process.env.AWS_REGION });
 
 export const handler = async (event) => {
+  const screenshotKey = "screenshot.png";
+
   try {
     const browser = await puppeteer.launch({
       args: chromium.args,
@@ -13,6 +18,13 @@ export const handler = async (event) => {
     const page = await browser.newPage();
     await page.goto("https://familyfunmap.ch/");
     const buffer = await page.screenshot();
+
+    const command = new PutObjectCommand({
+      Bucket: process.env.OUTPUT_BUCKET_NAME,
+      Key: screenshotKey,
+      Body: buffer,
+    });
+    await s3Client.send(command);
 
     return {
       statusCode: 200,
